bb56b517f67442ef321f7b62690945c5f8a038f7
Demétrios Reis Costa <demetriosreis1@gmail.com>
2022-04-16 21:14:43 -0300

    up2

---
+++ scripts/SQLQuery2.sql
@@ -0,0 +1,18 @@
+CREATE TABLE alunos (
+num_matricula int,
+nome varchar(50),
+segundo_nome varchar(50),
+cidade varchar(100),
+nota_fina int,
+sexo char
+)
+
+INSERT INTO alunos(num_matricula, nome,
+	segundo_nome, cidade, nota_fina, sexo) VALUES
+	(1, 'mikael', 'Richard', 'mcz', 9, 'F')
+
+select * from alunos
+
+
+
+
