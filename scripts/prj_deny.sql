3302e57fbff93c209bc03b34c027bda2f77b9b90
Demétrios Reis Costa <demetriosreis1@gmail.com>
2022-04-24 14:39:48 -0300

    add v1 deny

---
+++ scripts/prj_deny.sql
@@ -0,0 +1,7 @@
+DENY INSERT ON Jogadores TO RobertoCarlos
+
+INSERT INTO Jogadores VALUES ('Pelé', 'centranvante', 'M', 'Brasileiro')
+
+DENY SELECT ON Jogadores TO RobertoCarlos
+
+SELECT * FROM Jogadores
